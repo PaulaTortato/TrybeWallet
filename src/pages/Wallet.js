d086ebdb43c022a1714e0ae1fc1fd4a0ec58d515
Paula <paulacwtortato@gmail.com>
2022-02-09 17:22:45 -0300

    Desafio 2 - Erros de lint ajustados

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Wallet extends React.Component {
   render() {
     return (
-    <div>
-      TrybeWallet
-    </div>
+      <div>
+        TrybeWallet
+      </div>
     );
   }
 }
