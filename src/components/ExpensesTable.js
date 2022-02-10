57d578b81588a2f32b78a9ca62c5e0de7cce0bf7
Paula <paulacwtortato@gmail.com>
2022-02-10 18:07:58 -0300

    Desafio 7 - Dados da tabela sincronizados com o estado

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -1,25 +1,61 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class ExpensesTable extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
+      // Referência para tabelas em js: https://www.w3schools.com/html/html_tables.asp
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{Number(expense.value).toFixed(2)}</td>
+              {/* Referência para função substring: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substring */}
+              <td>
+                {expense.exchangeRates[expense.currency].name.includes('/')
+                  ? expense.exchangeRates[expense.currency].name.substring(0,
+                    expense.exchangeRates[expense.currency].name.indexOf('/'))
+                  : expense.exchangeRates[expense.currency].name}
+              </td>
+              <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+              <td>
+                {Number(expense.exchangeRates[expense.currency].ask * expense.value)
+                  .toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td />
+            </tr>
+          )) }
+        </tbody>
       </table>
     );
   }
 }
 
-export default ExpensesTable;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ExpensesTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(ExpensesTable);
